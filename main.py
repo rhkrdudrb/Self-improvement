e3a2d8699453277ec32554fad67430916f017616
영규 <78011285+rhkrdudrb@users.noreply.github.com>
2022-10-04 13:29:47 +0000

    2022-10-04  코드업 파이썬 기본 100제 51~70 기록 및 정리

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-a,b = map(int,input().split())
-if a != b:
-  print("True")
+a = int(input())
+
+if (a == 12 or a == 1 or a == 2):
+    print("winter")
+elif (a == 3 or a == 4 or a == 5):
+    print("spring")
+elif ( a == 6 or a == 7 or a == 8):
+    print("summer")
 else:
- print("False")
-  
-  
+    print("fall")
 
-
+  
