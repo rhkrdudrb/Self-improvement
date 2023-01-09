5033dfb836e43071a97ca4b494f522232fffcbce
영규 <78011285+rhkrdudrb@users.noreply.github.com>
2023-01-09 12:45:53 +0000

    Do'IT 코테 2023-01-09 코로나 이슈(열남..) 이유로 01~45P  45/533

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-num = int(input())
-numlist = list(map(int,input().split()))
-print(min(numlist))
+n, m = map(int, input().split())
+numbers = list(map(int, input().split()))
+prefix_sum = [0]
+temp = 0
+
+for i in numbers:
+   temp = temp + i
+   prefix_sum.append(temp)
+
+for i in range(m):
+  s,e = map(int, input().split())
+  print(prefix_sum[e] - prefix_sum[s-1])
