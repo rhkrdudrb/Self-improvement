8099b4c60431a4d3433966b7dcccdedcf10bc162
영규 <78011285+rhkrdudrb@users.noreply.github.com>
2023-01-11 12:11:47 +0000

    Do'IT 코테 2023-01-11 코로나 이슈(열남..) 이유로 01~48P 48/533

--- Python/Do'it03.py
+++ Python/Do'it03.py
@@ -1,5 +1,5 @@
 #구간 합 구하기 1 백준 11659번 복습필요
-n, m = map(int, input().split())
+n, m = map(int, input().split()) #숫자갯수,질의갯수
 numbers = list(map(int, input().split()))
 prefix_sum = [0]
 temp = 0
@@ -9,5 +9,5 @@ for i in numbers:
    prefix_sum.append(temp)
 
 for i in range(m):
-  s,e = map(int, input().split())
+  s,e = map(int, input().split()) #질의 범위 받기
   print(prefix_sum[e] - prefix_sum[s-1])
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-n, m = map(int, input().split())
-numbers = list(map(int, input().split()))
-prefix_sum = [0]
-temp = 0
-
-for i in numbers:
-   temp = temp + i
-   prefix_sum.append(temp)
-
-for i in range(m):
-  s,e = map(int, input().split())
-  print(prefix_sum[e] - prefix_sum[s-1])
+n,m = map(int ,input().split())
+numbers = list(map (int,input().split()))
+psum = [0]
+temp = 0 
+for i in numbers :
+  temp = temp + i
+  psum.append(temp)
+for i in range(m) :
+  s,l = map(int,input().split())
+  print(psum[l] - psum[s - 1])
+  
